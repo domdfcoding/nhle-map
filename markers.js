bf4f5d38ab528bd066a6727c1c67f98271277838
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-04-03 08:42:51 +0100

    Use bootstrap modal for loading progress bar

--- markers.js
+++ markers.js
@@ -56,22 +56,36 @@ function loadMarkers(latitude, longitide) {
 	}
 
 	var script = document.createElement('script');
+
+	var markerList = [];
+
+	progress.addEventListener('shown.bs.modal', event => {
+		if (loaded_ids.includes(id)) {
+			console.log(`Markers already loaded for ${latitude}N ${longitide}E`);
+			return
+		}
+		
+		markers.addLayers(markerList);
+		console.log('end clustering: ' + window.performance.now());
+
+		loaded_ids.push(id);
+	}, { once: true })
+
 	script.onload = function () {
+		console.log("Script loaded")
 		if (loaded_ids.includes(id)) {
 			console.log(`Markers already loaded for ${latitude}N ${longitide}E`);
 			return
 		}
 
-		var markerList = [];
 
 		console.log('start creating markers: ' + window.performance.now());
 		addMarkers(window["addressPoints" + id], markerList);
 
 		console.log('start clustering: ' + window.performance.now());
-		markers.addLayers(markerList);
-		console.log('end clustering: ' + window.performance.now());
+		// disable_interaction();
 
-		loaded_ids.push(id);
+		modal.show()
 
 	};
 
@@ -117,15 +131,18 @@ function getClusterRadius(zoom) {
 
 
 function updateProgressBar(processed, total, elapsed, layersArray) {
-	if (elapsed > 1000) {
-		// if it takes more than a second to load, display the progress bar:
-		progress.style.display = 'block';
-		progressBar.style.width = Math.round(processed / total * 100) + '%';
-	}
-
+	// if (elapsed > 1000) {
+	// if it takes more than a second to load, display the progress bar:
+	// 	if (elapsed > 0) {
+	// 		disable_interaction();
+	// 		modal.show()
+	// }
+	progressBar.style.width = Math.round(processed / total * 100) + '%';
+	// }
 	if (processed === total) {
 		// all markers processed - hide the progress bar:
-		progress.style.display = 'none';
+		modal.hide()
+		// enable_interaction();
 	}
 }
 
