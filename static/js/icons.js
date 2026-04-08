ef5431499b8407271e6b056fc5fb4301f7f30b9a
domdfcoding <dominic@davis-foster.co.uk>
2026-04-08 19:21:20 +0000

    Deploying to gh-pages from @ domdfcoding/nhle-map@74d412581539f8cbe9a5ea5990e6306dc2e50d35 🚀

---
+++ static/js/icons.js
@@ -0,0 +1,23 @@
+var battlefieldsIcon = L.ExtraMarkers.icon(
+{"innerHTML": "<img src='static/img/Challenge_Icon.svg' style='margin: 8px'>", "markerColor": "orange"}
+);
+
+var listedBuildingsIcon = L.ExtraMarkers.icon(
+{"icon": "fa-building", "markerColor": "blue", "prefix": "fa"}
+);
+
+var parksAndGardensIcon = L.ExtraMarkers.icon(
+{"icon": "fa-tree", "markerColor": "green", "prefix": "fa"}
+);
+
+var protectedWreckSitesIcon = L.ExtraMarkers.icon(
+{"icon": "fa-anchor", "markerColor": "purple", "prefix": "fa"}
+);
+
+var scheduledMonumentsIcon = L.ExtraMarkers.icon(
+{"icon": "fa-monument", "markerColor": "red", "prefix": "fa"}
+);
+
+var deDesignatedIcon = L.ExtraMarkers.icon(
+{"icon": "fa-ban", "markerColor": "black", "prefix": "fa"}
+);
