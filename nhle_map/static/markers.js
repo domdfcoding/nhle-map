3ef4bc77ef585fd26a7b03683f163f14de4f1bb4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-04-10 21:26:18 +0100

    Refactor load_new_markers into a replacement loadMarkers function.

--- nhle_map/static/markers.js
+++ nhle_map/static/markers.js
@@ -2,7 +2,6 @@ function load_new_markers() {
 	const bounds = map.getBounds();
 	var latitudes = range(Math.floor(bounds.getSouth()), Math.floor(bounds.getNorth()) + 1, 1);
 	var longitides = range(Math.floor(bounds.getWest()), Math.floor(bounds.getEast()) + 1, 1);
-	var markerList = [];
 	var chunkIDs = [];
 
 	latitudes.forEach(function(latitude) {
@@ -20,44 +19,48 @@ function load_new_markers() {
 	});
 
 	progress.addEventListener('shown.bs.modal', event => {
-		var addedChunkIDs = [];
-		var scriptPromises = [];
-
-		console.log('Loading scripts', chunkIDs);
-		chunkIDs.forEach(function(id) {
-			var script = document.createElement('script');
-			scriptPromises.push(new Promise((resolve, reject) => {
-				script.onload = function() {
-					console.log('Script', id, 'loaded');
-					resolve();
-				};
-			}));
-			script.src = `data/listed_buildings_${id}.js`;
-			document.head.appendChild(script);
-		});
-
-		Promise.all(scriptPromises).then((values) => {
-			console.log('Adding markers for ids', chunkIDs);
-			chunkIDs.forEach(function(id) {
-				if (loaded_ids.includes(id)) {
-					console.log(`Markers already loaded for ID ${id}`);
-				} else {
-					console.log('Accessing JS variable', 'listedBuildings' + id);
-					addMarkers(window['listedBuildings' + id], markerList, listedBuildingsIcon);
-					addedChunkIDs.push(id);
-				}
-			});
-
-			marker_cluster_listed_buildings.addLayers(markerList);
-			loaded_ids.push(...addedChunkIDs);
-		});
+		loadMarkers(chunkIDs);
 	}, { once: true });
 
-	// TODO: move script/promise code inside post-modal.show() block so map is blocked during
 	if (chunkIDs.length > 0) {
 		console.log('Showing progressbar');
 		modal.show();
 	}
+}
+
+function loadMarkers(chunkIDs) {
+	var scriptPromises = [];
+	var markerList = [];
+	var addedChunkIDs = [];
+
+	console.log('Loading scripts', chunkIDs);
+	chunkIDs.forEach(function(id) {
+		var script = document.createElement('script');
+		scriptPromises.push(new Promise((resolve, reject) => {
+			script.onload = function() {
+				console.log('Script', id, 'loaded');
+				resolve();
+			};
+		}));
+		script.src = `data/listed_buildings_${id}.js`;
+		document.head.appendChild(script);
+	});
+
+	Promise.all(scriptPromises).then((values) => {
+		console.log('Adding markers for ids', chunkIDs);
+		chunkIDs.forEach(function(id) {
+			if (loaded_ids.includes(id)) {
+				console.log(`Markers already loaded for ID ${id}`);
+			} else {
+				console.log('Accessing JS variable', 'listedBuildings' + id);
+				addMarkers(window['listedBuildings' + id], markerList, listedBuildingsIcon);
+				addedChunkIDs.push(id);
+			}
+		});
+
+		marker_cluster_listed_buildings.addLayers(markerList);
+		loaded_ids.push(...addedChunkIDs);
+	});
 }
 
 function addMarkers(points, markerList, icon) {
@@ -86,48 +89,8 @@ function lookup_id(latitude, longitide) {
 	return id;
 }
 
-function loadMarkers(latitude, longitide) {
-	var id = lookup_id(latitude, longitide);
-	if (loaded_ids.includes(id)) {
-		console.log(`Markers already loaded for ${latitude}N ${longitide}E`);
-		return;
-	}
 
-	var script = document.createElement('script');
 
-	var markerList = [];
-
-	progress.addEventListener('shown.bs.modal', event => {
-		if (loaded_ids.includes(id)) {
-			console.log(`Markers already loaded for ${latitude}N ${longitide}E`);
-			return;
-		}
-
-		marker_cluster_listed_buildings.addLayers(markerList);
-		console.log('end clustering: ' + window.performance.now());
-
-		loaded_ids.push(id);
-	}, { once: true });
-
-	script.onload = function() {
-		console.log('Script loaded');
-		if (loaded_ids.includes(id)) {
-			console.log(`Markers already loaded for ${latitude}N ${longitide}E`);
-			return;
-		}
-
-		console.log('start creating markers: ' + window.performance.now());
-		addMarkers(window['listedBuildings' + id], markerList, listedBuildingsIcon);
-
-		console.log('start clustering: ' + window.performance.now());
-		// disable_interaction();
-
-		modal.show();
-	};
-
-	script.src = `data/listed_buildings_${id}.js`;
-	document.head.appendChild(script);
-}
 
 // https://github.com/jashkenas/underscore/blob/master/underscore.js
 // MIT
