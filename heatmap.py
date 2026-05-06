63e55d6398ef35fa0d836ff281144cbb2f5140d3
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-05-06 14:13:15 +0100

    Move some logic to domdf-folium-tools

--- heatmap.py
+++ heatmap.py
@@ -2,8 +2,8 @@
 import json
 
 # 3rd party
+import domdf_folium_tools.heatmap
 import folium
-import folium.plugins
 import geopandas
 import numpy
 import pandas
@@ -20,178 +20,41 @@ from nhle_map.utils import copy_static_files
 set_branca_random_seed("NHLE")
 
 
-class HeatMapWithTime(folium.plugins.HeatMapWithTime):
-	default_js = folium.plugins.HeatMapWithTime.default_js + [("nhle_heatmap", "static/js/heatmap.js")]
-
-	_template = Template(
-			"""
-        {% macro script(this, kwargs) %}
-
-            var times = {{this.times}};
-
-            {{this._parent.get_name()}}.timeDimension = L.timeDimension(
-                {times : times, currentTime: new Date(1)}
-            );
-
-            var {{this._control_name}} = new L.Control.TimeDimensionCustom({{this.index}}, {
-                autoPlay: {{this.auto_play}},
-                backwardButton: {{this.backward_button}},
-                displayDate: {{this.display_index}},
-                forwardButton: {{this.forward_button}},
-                limitMinimumRange: {{this.limit_minimum_range}},
-                limitSliders: {{this.limit_sliders}},
-                loopButton: {{this.loop_button}},
-                maxSpeed: {{this.max_speed}},
-                minSpeed: {{this.min_speed}},
-                playButton: {{this.play_button}},
-                playReverseButton: {{this.play_reverse_button}},
-                position: "{{this.position}}",
-                speedSlider: {{this.speed_slider}},
-                speedStep: {{this.speed_step}},
-                styleNS: "{{this.style_NS}}",
-                timeSlider: {{this.time_slider}},
-                timeSliderDragUpdate: {{this.time_slider_drag_update}},
-                timeSteps: {{this.index_steps}}
-                })
-                .addTo({{this._parent.get_name()}});
-
-                {# var {{this.get_name()}} = new TDHeatmap({{this.data}}, #}
-                var {{this.get_name()}} = new TDHeatmap(heatmapData,
-                {heatmapOptions: {
-                        radius: {{this.radius}},
-                        blur: {{this.blur}},
-                        minOpacity: {{this.min_opacity}},
-                        maxOpacity: {{this.max_opacity}},
-                        scaleRadius: {{this.scale_radius}},
-                        useLocalExtrema: {{this.use_local_extrema}},
-                        defaultWeight: 1,
-                        {% if this.gradient %}gradient: {{ this.gradient }}{% endif %}
-                    }
-                });
-
-        {% endmacro %}
-        """,
-			)
-
-
-class HeatLayerWithTime(folium.plugins.HeatMapWithTime):
-
-	def __init__(
-			self,
-			data,
-			index=None,
-			name=None,
-			radius=25,
-			blur=15,
-			min_opacity=0.05,
-			max_opacity=0.6,
-			scale_radius=False,
-			gradient=None,
-			use_local_extrema=False,
-			auto_play=False,
-			display_index=True,
-			index_steps=1,
-			min_speed=0.1,
-			max_speed=10,
-			speed_step=0.1,
-			position="bottomleft",
-			overlay=True,
-			control=True,
-			show=True,  # TODO: max
-			):
-		super().__init__(
-				data=data,
-				index=index,
-				name=name,
-				radius=radius,
-				blur=blur,
-				min_opacity=min_opacity,
-				max_opacity=max_opacity,
-				scale_radius=scale_radius,
-				gradient=gradient,
-				use_local_extrema=use_local_extrema,
-				auto_play=auto_play,
-				display_index=display_index,
-				index_steps=index_steps,
-				min_speed=min_speed,
-				max_speed=max_speed,
-				speed_step=speed_step,
-				position=position,
-				overlay=overlay,
-				control=control,
-				show=show,
-				)
-
-	default_js = [
-			(
-					"iso8601",
-					"https://cdn.jsdelivr.net/npm/iso8601-js-period@0.2.1/iso8601.min.js",
-					),
-			(
-					"leaflet.timedimension.min.js",
-					"https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.1/dist/leaflet.timedimension.min.js",
-					),
-			(
-					"leaflet-heat.js",
-					"https://leaflet.github.io/Leaflet.heat/dist/leaflet-heat.js",  # TODO: jsdelivr
-					),
+class HeatMapWithTime(domdf_folium_tools.heatmap.HeatMapWithTime):
+	default_js = domdf_folium_tools.heatmap.HeatMapWithTime.default_js[:-1] + [
+			("domdf_folium_tools_js", "static/js/domdf-folium-tools.js"),
 			("nhle_heatmap", "static/js/heatmap.js"),
 			]
-	default_css = [
-			(
-					"leaflet.timedimension.control.min.css",
-					"https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.1/dist/leaflet.timedimension.control.css",
-					),
-			]
 
 	_template = Template(
 			"""
-        {% macro script(this, kwargs) %}
+		{% macro script(this, kwargs) %}
 
-            var times = {{this.times}};
+			var times = {{this.times}};
 
-            {{this._parent.get_name()}}.timeDimension = L.timeDimension(
-                {times : times, currentTime: new Date(1)}
-            );
+			{{this._parent.get_name()}}.timeDimension = L.timeDimension(
+				{times : times, currentTime: new Date(1)}
+			);
 
-            var {{this._control_name}} = new L.Control.TimeDimensionCustom({{this.index}}, {
-                autoPlay: {{this.auto_play}},
-                backwardButton: {{this.backward_button}},
-                displayDate: {{this.display_index}},
-                forwardButton: {{this.forward_button}},
-                limitMinimumRange: {{this.limit_minimum_range}},
-                limitSliders: {{this.limit_sliders}},
-                loopButton: {{this.loop_button}},
-                maxSpeed: {{this.max_speed}},
-                minSpeed: {{this.min_speed}},
-                playButton: {{this.play_button}},
-                playReverseButton: {{this.play_reverse_button}},
-                position: "{{this.position}}",
-                speedSlider: {{this.speed_slider}},
-                speedStep: {{this.speed_step}},
-                styleNS: "{{this.style_NS}}",
-                timeSlider: {{this.time_slider}},
-                timeSliderDragUpdate: {{this.time_slider_drag_update}},
-                timeSteps: {{this.index_steps}}
-                })
-                .addTo({{this._parent.get_name()}});
+			var {{this._control_name}} = new L.Control.TimeDimensionHeatmap(
+				{{this.index | tojson}},
+				{{ this.control_options | tojson(indent=20) }},
+			).addTo({{this._parent.get_name()}});
 
-                var {{this.get_name()}} = new TDHeatLayer(heatmapData,
-                {heatmapOptions: {
-                        radius: {{this.radius}},
-                        blur: {{this.blur}},
-                        minOpacity: {{this.min_opacity}},
-                        // maxOpacity: {{this.max_opacity}},
-                        // scaleRadius: {{this.scale_radius}},
-                        // useLocalExtrema: {{this.use_local_extrema}},
-                        // defaultWeight: 1,
-                        {% if this.gradient %}gradient: {{ this.gradient }}{% endif %}
-                    }
-                });
+			var {{this.get_name()}} = new TDHeatmapCustom(
+				heatmapData,
+				{heatmapOptions: {{ this.heatmap_options|tojson(indent=20) }}},
+			);
 
-        {% endmacro %}
-        """,
+		{% endmacro %}
+		""".replace('\t', "    "),
 			)
+
+
+class HeatLayerWithTime(domdf_folium_tools.heatmap.HeatLayerWithTime):
+	default_js = domdf_folium_tools.heatmap.HeatLayerWithTime.default_js[:-1] + [
+			("domdf_folium_tools_js", "static/js/domdf-folium-tools.js"),
+			]
 
 
 dataset = constants.LISTED_BUILDINGS
@@ -212,9 +75,8 @@ points = []
 for timestamp, group in gdf.groupby(pandas.Grouper(key="ListDate", freq="YE")):
 	points = []
 	for item in group.to_dict("records"):
-		coords = list(map(float, numpy.round(item["geometry"].bounds[:2], 10)))[::-1]
-		points.append([*coords, 0.00001])  # TODO: divide 300,000 by area of England
-	# points.extend(list(map(float, numpy.round(item["geometry"].bounds[:2], 10)))[::-1] for item in group.to_dict("records"))
+		coords = tuple(map(float, numpy.round(item["geometry"].bounds[:2], 10)))[::-1]
+		points.append((*coords, 0.00001))  # TODO: divide 300,000 by area of England
 	if points:
 		index.append(timestamp.strftime("%Y"))
 		heatmap_data.append(points)
--- nhle_map/static/heatmap.js
+++ nhle_map/static/heatmap.js
@@ -3,76 +3,74 @@
 *  MIT Licenced
 */
 
-if (typeof HeatmapOverlay !== 'undefined') {
-	var HMO = HeatmapOverlay.extend({
-		_update: function() {
-			var bounds, zoom, scale;
-			var generatedData = { max: this._max, min: this._min, data: [] };
+var HMO = HeatmapOverlay.extend({
+	_update: function() {
+		var bounds, zoom, scale;
+		var generatedData = { max: this._max, min: this._min, data: [] };
 
-			bounds = this._map.getBounds();
-			zoom = this._map.getZoom();
-			// scale = Math.pow(2, zoom);
-			//   scale = (1/zoom) * 7  // 7 is default zoom; get from map settings
-			// scale = zoom / 7  // 7 is default zoom; get from map settings
-			scale = Math.pow(zoom / 7, 2); // 7 is default zoom; get from map settings
-			console.log('Zoom:', zoom, '  Scale:', scale);
+		bounds = this._map.getBounds();
+		zoom = this._map.getZoom();
+		// scale = Math.pow(2, zoom);
+		//   scale = (1/zoom) * 7  // 7 is default zoom; get from map settings
+		// scale = zoom / 7  // 7 is default zoom; get from map settings
+		scale = Math.pow(zoom / 7, 2); // 7 is default zoom; get from map settings
+		console.log('Zoom:', zoom, '  Scale:', scale);
 
-			if (this._data.length == 0) {
-				if (this._heatmap) {
-					this._heatmap.setData(generatedData);
-				}
-				return;
+		if (this._data.length == 0) {
+			if (this._heatmap) {
+				this._heatmap.setData(generatedData);
 			}
+			return;
+		}
 
-			var latLngPoints = [];
-			var radiusMultiplier = this.cfg.scaleRadius ? scale : 1;
-			var localMax = 0;
-			var localMin = 0;
-			var valueField = this.cfg.valueField;
-			var len = this._data.length;
+		var latLngPoints = [];
+		var radiusMultiplier = this.cfg.scaleRadius ? scale : 1;
+		var localMax = 0;
+		var localMin = 0;
+		var valueField = this.cfg.valueField;
+		var len = this._data.length;
 
-			while (len--) {
-				var entry = this._data[len];
-				var value = entry[valueField];
-				var latlng = entry.latlng;
+		while (len--) {
+			var entry = this._data[len];
+			var value = entry[valueField];
+			var latlng = entry.latlng;
 
-				// we don't wanna render points that are not even on the map ;-)
-				if (!bounds.contains(latlng)) {
-					continue;
-				}
-				// local max is the maximum within current bounds
-				localMax = Math.max(value, localMax);
-				localMin = Math.min(value, localMin);
-
-				var point = this._map.latLngToContainerPoint(latlng);
-				var latlngPoint = { x: Math.round(point.x), y: Math.round(point.y) };
-				latlngPoint[valueField] = value;
-
-				var radius;
-
-				if (entry.radius) {
-					radius = entry.radius * radiusMultiplier;
-				} else {
-					radius = (this.cfg.radius || 2) * radiusMultiplier;
-				}
-				latlngPoint.radius = radius;
-				latLngPoints.push(latlngPoint);
+			// we don't wanna render points that are not even on the map ;-)
+			if (!bounds.contains(latlng)) {
+				continue;
 			}
-			if (this.cfg.useLocalExtrema) {
-				generatedData.max = localMax;
-				generatedData.min = localMin;
+			// local max is the maximum within current bounds
+			localMax = Math.max(value, localMax);
+			localMin = Math.min(value, localMin);
+
+			var point = this._map.latLngToContainerPoint(latlng);
+			var latlngPoint = { x: Math.round(point.x), y: Math.round(point.y) };
+			latlngPoint[valueField] = value;
+
+			var radius;
+
+			if (entry.radius) {
+				radius = entry.radius * radiusMultiplier;
+			} else {
+				radius = (this.cfg.radius || 2) * radiusMultiplier;
 			}
+			latlngPoint.radius = radius;
+			latLngPoints.push(latlngPoint);
+		}
+		if (this.cfg.useLocalExtrema) {
+			generatedData.max = localMax;
+			generatedData.min = localMin;
+		}
 
-			generatedData.data = latLngPoints;
+		generatedData.data = latLngPoints;
 
-			this._heatmap.setData(generatedData);
-		},
-	});
-}
+		this._heatmap.setData(generatedData);
+	},
+});
 
-var TDHeatmap = L.TimeDimension.Layer.extend({
+var TDHeatmapCustom = L.TDHeatmap.extend({
 	initialize: function(data, options) {
-		var heatmapCfg = {
+		const heatmapCfg = {
 			radius: 15,
 			blur: 0.8,
 			maxOpacity: 1.,
@@ -84,7 +82,7 @@ var TDHeatmap = L.TimeDimension.Layer.extend({
 			defaultWeight: 1,
 			...options.heatmapOptions || {},
 		};
-		var layer = new HMO(heatmapCfg);
+		const layer = new HMO(heatmapCfg);
 		L.TimeDimension.Layer.prototype.initialize.call(this, layer, options);
 		this._currentLoadedTime = 0;
 		this._currentTimeData = {
@@ -92,85 +90,5 @@ var TDHeatmap = L.TimeDimension.Layer.extend({
 		};
 		this.data = data;
 		this.defaultWeight = heatmapCfg.defaultWeight || 1;
-	},
-	onAdd: function(map) {
-		L.TimeDimension.Layer.prototype.onAdd.call(this, map);
-		map.addLayer(this._baseLayer);
-		if (this._timeDimension) {
-			this._getDataForTime(this._timeDimension.getCurrentTime());
-		}
-	},
-	_onNewTimeLoading: function(ev) {
-		this._getDataForTime(ev.time);
-		return;
-	},
-	isReady: function(time) {
-		return (this._currentLoadedTime == time);
-	},
-	_update: function() {
-		this._baseLayer.setData(this._currentTimeData);
-		return true;
-	},
-	_getDataForTime: function(time) {
-		delete this._currentTimeData.data;
-		this._currentTimeData.data = [];
-		console.log('time=', time);
-		// var data = this.data[time-1];
-		var data = Array.prototype.concat(...this.data.slice(0, time));
-		for (var i = 0; i < data.length; i++) {
-			this._currentTimeData.data.push({
-				lat: data[i][0],
-				lng: data[i][1],
-				count: data[i].length > 2 ? data[i][2] : this.defaultWeight,
-			});
-		}
-		this._currentLoadedTime = time;
-		if (this._timeDimension && time == this._timeDimension.getCurrentTime() && !this._timeDimension.isLoading()) {
-			this._update();
-		}
-		this.fire('timeload', {
-			time: time,
-		});
-	},
-});
-
-var TDHeatLayer = TDHeatmap.extend({
-	initialize: function(data, options) {
-		var heatmapCfg = {
-			minOpacity: 0.05,
-			maxZoom: 18,
-			radius: 25,
-			blur: 15,
-			max: 1.0,
-			...options.heatmapOptions || {},
-		};
-		var layer = new L.HeatLayer([], heatmapCfg);
-		L.TimeDimension.Layer.prototype.initialize.call(this, layer, options);
-		this._currentLoadedTime = 0;
-		this._currentTimeData = {
-			data: [],
-		};
-		this.data = data;
-		// this.defaultWeight = heatmapCfg.defaultWeight || 1;
-	},
-	_update: function() {
-		console.log(this._currentTimeData.data);
-		this._baseLayer.setLatLngs(this._currentTimeData.data);
-		return true;
-	},
-});
-
-L.Control.TimeDimensionCustom = L.Control.TimeDimension.extend({
-	initialize: function(index, options) {
-		options.playerOptions = {
-			buffer: 1,
-			minBufferReady: -1,
-			...options.playerOptions || {},
-		};
-		L.Control.TimeDimension.prototype.initialize.call(this, options);
-		this.index = index;
-	},
-	_getDisplayDateFormat: function(date) {
-		return this.index[date.getTime() - 1];
 	},
 });
